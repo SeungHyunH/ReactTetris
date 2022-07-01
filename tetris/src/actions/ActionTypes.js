0e880b8636034a6debd5e2de519f5d1bdd480ef3
SeungHyunH <vkflek1232@naver.com>
2022-07-01 11:05:17 +0900

    초기화

--- tetris/src/actions/ActionTypes.js
+++
@@ -1,6 +0,0 @@
-export const MOVE_LEFT = "MOVE_LEFT";
-export const MOVE_RIGHT = "MOVE_RIGHT";
-export const MOVE_UP = "MOVE_UP";
-export const MOVE_DOWN = "MOVE_DOWN";
-export const SET_POS = "SET_POS";
-export const SET_BOARD = "SET_BOARD";
